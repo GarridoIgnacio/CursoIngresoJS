18e071d651888ce27e8bb63459c93c02bcf9019d
unknown <nacho.gdo@gmail.com>
2021-07-08 00:03:53 -0300

    resuelto

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -13,7 +13,7 @@ function mostrar()
     // guardo en la variable nombre el texto que escribio el usuario dentro del la ventana prompt
 	nombre = prompt ("Igrese su Nombre");
 
-	// copio el nombre que tengo guardado en la variable nombre que tengo dentro de la caja de texto embebida en la pagina html
+	// copio el nombre que tengo guardado en la variable "nombre" que tengo dentro de la caja de texto embebida en la pagina html
 	document.getElementById("txtIdNombre").value = nombre;
 
 }
--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -11,19 +11,19 @@ function sumar()
 	var num2;
 	var resultado;
    
-	/* UNA MANERA
+	// UNA MANERA
 	//traigo los datos del imput del html para guardalso en la variable osea lo que se escribe en el cuadro de texto
 	num1 = document.getElementById("txtIdNumeroUno").value;
     num2 = document.getElementById("txtIdNumeroDos").value;
  
 	//con el parseInt convierto los datos tomados de txt a num, filtra del 1 al 9, numeros enteros para numors con decimales se usa "parseFloat"
     num1 = parseInt(num1);
-	num2 = parseInt(num2);*/
+	num2 = parseInt(num2);
 
 	// OTRA forma es..
 
-    num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
-    num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+    /*num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+    num2 = parseInt(document.getElementById("txtIdNumeroDos").value);*/
 	
 	// sumo las dos variables ahora numericas y guardo en el resulato en la variable resultado
 	resultado = num1 + num2;
